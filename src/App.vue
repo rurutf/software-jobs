f0b10cd1811b4a75c23f1ae5b9d013bd0cf96c14
Inna Pavlyutenkova <i.pavlyutenkova@devspark>
2022-05-15 12:36:51 +0300

    add jobs list component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
     <div class="app">
-        <p>{{ name }}</p>
+        <JobsList :jobs="jobs" />
     </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref, reactive, toRefs} from "vue";
+import Job from "@/types/Job";
+import JobsList from "@/components/JobsList.vue";
 
 export default defineComponent({
     name: 'App',
-    components: {},
-    data() {
+    components: {
+        JobsList,
+    },
+    setup() {
+        const jobs = ref<Job[]>([
+            { title: 'Frontend', location: 'Moscow', salary: 4000, id: '1' },
+            { title: 'Frontend', location: 'Moscow', salary: 4000, id: '2' },
+            { title: 'Frontend', location: 'Moscow', salary: 4000, id: '3' },
+            { title: 'Frontend', location: 'Moscow', salary: 4000, id: '4' },
+            { title: 'Frontend', location: 'Moscow', salary: 4000, id: '5' }
+        ])
+
         return {
-            name: 'Link',
+            jobs,
         }
-    }
+    },
 })
 </script>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="job-list">
+        <ul>
+            <li
+                v-for="job in jobs"
+                :key="job.id"
+            class="job-list__item">
+                <h2 class="job-list__title">{{ job.title }} in {{ job.location }}</h2>
+                <div class="job-list__salary">
+                    <p class="job-list__salary-item">{{ job.salary }} dollars</p>
+                </div>
+                <div class="job-list__description">
+                    <p>lorem ipsum</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+import Job from "@/types/Job";
+
+export default defineComponent({
+    props: {
+        jobs: {
+            type: Array as PropType<Job[]>,
+            required: true,
+        },
+    },
+    setup() {
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss">
+.job-list {
+    max-width: 960px;
+    margin: 40px auto;
+
+    ul {
+        padding: 0;
+    }
+
+    &__item {
+        margin: 16px 0;
+        padding: 16px;
+        background: #ffffff;
+        border-radius: 10px;
+        list-style-type: none;
+    }
+
+    &__title {
+        margin: 0 0 10px;
+        text-transform: capitalize;
+    }
+
+    &__salary {
+        display: flex;
+    }
+
+    &__salary-img {
+        width: 30px;
+    }
+
+    &__salary-item {
+        margin: 10px 8px;
+        color: #17bf66;
+        font-weight: bold;
+    }
+}
+</style>
